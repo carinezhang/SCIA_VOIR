d629a1aeac678f8e940053c737b6e4acc1694133
Guillaume Drapala-Bizouarn <guidrapbiz@hotmail.fr>
2019-02-08 00:11:09 +0100

    Try few things

--- test_cpp.cpp
+++ test_cpp.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char **argv)
 {
-  cv::Mat ori = cv::imread("bd_projet_scia/damier/G0010566.JPG", cv::IMREAD_COLOR);
+  cv::Mat ori = cv::imread("bd_projet_scia/damier/G0010391.JPG", cv::IMREAD_COLOR);
   cv::Mat gray;
   cv::Mat img;
   auto height = ori.size().height;
@@ -18,17 +18,23 @@ int main(int argc, char **argv)
 
   cv::cvtColor(img, gray, CV_BGR2GRAY);
   cv::Size patternsize(8, 9);
+  cv::Size patternsize2(9, 8);
   std::vector<cv::Point2f> corners;
 
+  // SEARCH FOR A CHESSBOARD
   bool patternfound = findChessboardCorners(gray, patternsize, corners,
       cv::CALIB_CB_ADAPTIVE_THRESH + cv::CALIB_CB_NORMALIZE_IMAGE
       + cv::CALIB_CB_FAST_CHECK);
 
   std::cout << "CHESSBOARD found : " << patternfound << std::endl;
+  std::cout << "  coordonates found : " << corners << std::endl;
 
-  //if (patternfound)
-  //  cornerSubPix(gray, corners, cv::Size(11, 11), cv::Size(-1, -1),
-  //      cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+  if (patternfound)
+    cornerSubPix(gray, corners, cv::Size(11, 11), cv::Size(-1, -1),
+        cv::TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
+
+  std::cout << "============================================" << std::endl;
+  std::cout << "  coordonates found : " << corners << std::endl;
 
   drawChessboardCorners(img, patternsize, cv::Mat(corners), patternfound);
 
