2fea3d8fc67433b68abefcb091d166a5b3fcfee3
Guillaume Drapala-Bizouarn <guidrapbiz@hotmail.fr>
2019-02-08 05:56:41 +0100

    Echo args

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ source VOIR_zhang_d/bin/activate
 pip3 install imutils
 pip3 install opencv-python==3.4.2.16
 pip3 install opencv-contrib-python==3.4.2.16
+echo $@
 python3 part1.py $@
 python3 part2.py $@
 rm -fr ./VOIR_zhang_d
