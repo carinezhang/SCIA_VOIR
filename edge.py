8a6b5d4c7ccf1b502dfb4cc01e0ecff891ec2748
Carine Zhang <zh.carine@gmail.com>
2019-02-08 04:50:23 +0100

    detect if mire or chessboard is present

--- edge.py
+++ edge.py
@@ -2,11 +2,87 @@ import cv2
 import glob
 import numpy as np
 import imutils
+import json
+
+#import matplotlib
+#matplotlib.use("TkAgg")
+#from matplotlib import pyplot as plt
+
+def getSubImage(rect, src):
+    # Get center, size, and angle from rect
+    center, size, theta = rect
+    # Convert to int 
+    center, size = tuple(map(int, center)), tuple(map(int, size))
+    # Get rotation matrix for rectangle
+    M = cv2.getRotationMatrix2D( center, theta, 1)
+    # Perform rotation on src image
+    dst = cv2.warpAffine(src, M, src.shape[:2])
+    out = cv2.getRectSubPix(dst, size, center)
+    return out
+
+def find_if_close(cnt1,cnt2):
+    row1,row2 = cnt1.shape[0],cnt2.shape[0]
+    for i in range(row1):
+        for j in range(row2):
+            dist = np.linalg.norm(cnt1[i]-cnt2[j])
+            if abs(dist) < 50 :
+                return True
+            elif i==row1-1 and j==row2-1:
+                return False
+
+
+def feature_matching(img1, img2):
+        MIN_MATCH_COUNT = 5
+        #img_mire = cv2.imread(img_ref,0)          # queryImage
+        #img2 = cv2.imread(img,0) # trainImage
+        # Initiate SIFT detector
+        sift = cv2.xfeatures2d.SIFT_create()
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1,None)
+        kp2, des2 = sift.detectAndCompute(img2,None)
+        FLANN_INDEX_KDTREE = 1
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(des1,des2,k=2)
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m,n in matches:
+                if m.distance < 0.7*n.distance:
+                        good.append(m)
+
+        if len(good)>MIN_MATCH_COUNT:
+                return True
+                # src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+                # dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+                # M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+                # matchesMask = mask.ravel().tolist()
+                # h,w,d = img1.shape
+                # pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+                # dst = cv2.perspectiveTransform(pts,M)
+                # img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+                
+                # draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                #    singlePointColor = None,
+                #    matchesMask = matchesMask, # draw only inliers
+                #    flags = 2)
+                # img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+                #plt.imshow(img3, 'gray'),plt.show()
+
+        else:
+                print( "Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT) )
+                return False
+
 
 
 images = glob.glob('bd_projet_scia/mires/*')
+img_mire = cv2.imread('data/mire.png',0)    
+img_damier = cv2.imread('data/damier.png',0)    
 
+
+detected_or_not = {}
 for fname in images:
+        detected = False
         raw_image = cv2.imread(fname)
 
 
@@ -32,7 +108,8 @@ for fname in images:
         cv2.imshow('a', edges)
         cv2.waitKey(0)
 
-        cnts = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        edges_copy = edges.copy()
+        cnts = cv2.findContours(edges_copy, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
         screenCnt = None
@@ -56,30 +133,69 @@ for fname in images:
                 cv2.imshow("a", copy)
                 cv2.waitKey(0)
 
-                for contour in cnts:
-                        (x,y,w,h) = cv2.boundingRect(contour)
-                        cv2.rectangle(resized, (x,y), (x+w,y+h), (0,255,0), 2)
-                cv2.imshow("a", resized)
-                cv2.waitKey(0)
+                # LENGTH = len(cnts)
+                # status = np.zeros((LENGTH,1))
+
+                # for i,cnt1 in enumerate(cnts):
+                #         x = i    
+                #         if i != LENGTH-1:
+                #                 for j,cnt2 in enumerate(cnts[i+1:]):
+                #                         x = x+1
+                #                         dist = find_if_close(cnt1,cnt2)
+                #                         if dist == True:
+                #                                 val = min(status[i],status[x])
+                #                                 status[x] = status[i] = val
+                #                         else:
+                #                                 if status[x]==status[i]:
+                #                                         status[x] = i+1
+
+                # unified = []
+                # maximum = int(status.max())+1
+                # for i in range(maximum):
+                #         pos = np.where(status==i)[0]
+                #         if pos.size != 0:
+                #                 cont = np.vstack(cnts[i] for i in pos)
+                #                 hull = cv2.convexHull(cont)
+                #                 unified.append(hull)
+
+                # img_copy = resized.copy()
+                # cv2.drawContours(img_copy,unified,-1,(0,255,0),2)
+                # cv2.drawContours(edges_copy,unified,-1,255,-1)
+                # cv2.imshow("a", img_copy)
+                # cv2.waitKey(0)
+
+                # copy2 = resized.copy()
+                # for contour in unified:
+                #         #(x,y,w,h) = cv2.boundingRect(contour)
+                #         #cv2.rectangle(resized, (x,y), (x+w,y+h), (0,255,0), 2)
+                #         rect = cv2.minAreaRect(contour)
+                #         box = cv2.boxPoints(rect)
+                #         box = np.int0(box)
+                #         cv2.drawContours(copy2,[box],0,(0,0,255),2)
+                        
+                #         rect_detected = getSubImage(rect, resized)
+                #         cv2.imshow('a', rect_detected)
+                #         cv2.waitKey(0)
+                        
+                #         if feature_matching(rect_detected, img_damier):
+                #                 detected = True
+                #         if feature_matching(rect_detected, img_mire):
+                #                 detected = True
+                # cv2.imshow("a", copy2)
+                # cv2.waitKey(0)
+                
+
+                if feature_matching(resized, img_damier):
+                        detected = True
+                if feature_matching(resized, img_mire):
+                        detected = True
+        detected_or_not[fname] = detected
+
+
+        with open('result_part1.json', 'w') as fp:
+                json.dump(detected_or_not, fp)
 
 
 
-        # rho = 1  # distance resolution in pixels of the Hough grid
-        # theta = np.pi / 180  # angular resolution in radians of the Hough grid
-        # threshold = 10  # minimum number of votes (intersections in Hough grid cell)
-        # min_line_length = 40  # minimum number of pixels making up a line
-        # max_line_gap = 20  # maximum gap in pixels between connectable line segments
-        # line_image = np.copy(resized) * 0  # creating a blank to draw lines on
 
-        # # Run Hough on edge detected image
-        # # Output "lines" is an array containing endpoints of detected line segments
-        # lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-        #                 min_line_length, max_line_gap)
-        # if lines is not None:
-        #         for line in lines:
-        #                 for x1,y1,x2,y2 in line:
-        #                         cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
-        #         print(lines)
-        # lines_edges = cv2.addWeighted(resized, 0.8, line_image, 1, 0)
-        # cv2.imshow('a', line_image)
-        # cv2.waitKey(0)
+
