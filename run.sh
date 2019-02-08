dca2df0273b0e4ee06c237464c115c769017e424
Guillaume Drapala-Bizouarn <guidrapbiz@hotmail.fr>
2019-02-08 04:52:53 +0100

    Add opencv contrib library

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 python3 -m venv ./VOIR_zhang_d
 source VOIR_zhang_d/bin/activate
-pip install imutils opencv-python
+pip3 install imutils
+pip3 install opencv-python==3.4.2.16
+pip3 install opencv-contrib-python==3.4.2.16
 python3 edge.py
 rm -fr ./VOIR_zhang_d
